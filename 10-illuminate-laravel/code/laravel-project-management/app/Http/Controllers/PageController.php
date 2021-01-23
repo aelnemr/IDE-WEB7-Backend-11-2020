07adbc336e3b659360a09bfe2a84d82a5ab84330
Ahmed Elnemr <tiger52671@gmail.com>
2021-01-23 20:49:10 +0200

    Session 10

--- 10-illuminate-laravel/code/laravel-project-management/app/Http/Controllers/PageController.php
+++ 10-illuminate-laravel/code/laravel-project-management/app/Http/Controllers/PageController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class PageController extends Controller
 {
-    //
+    public function home()
+    {
+//        User::query()->create([
+//            'name' => 'user',
+//            'email' => 'user1@gmail.com',
+//            'password' => Hash::make('123456789'),
+//        ]);
+//
+//        $users = User::all();
+//        dd($users);
+
+        Project::query()->create([
+            'title' => 'Project One',
+            'description' => 'Describe'
+        ]);
+
+        dd(Project::all());
+        return "Home form controller";
+    }
 }
